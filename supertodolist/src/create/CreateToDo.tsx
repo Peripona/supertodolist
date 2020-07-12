e47b321ecf74eec3e52016f91b9ca94ba2c8d96d
Tarandeep Singh <peripona@yahoo.com>
2020-07-12 20:38:20 +0200

    todo list page

---
+++ supertodolist/src/create/CreateToDo.tsx
@@ -0,0 +1,36 @@
+import React, {FunctionComponent, KeyboardEvent, ReactElement, useState} from "react";
+
+export type priorityType = 'low' | 'medium' | 'high';
+
+interface Task {
+    title: string,
+    time: string,
+    priority: priorityType,
+}
+
+interface CreateToDoProps {
+    task: string
+}
+
+export const CreateToDo: FunctionComponent = (): ReactElement => {
+    const initialState: Array<string> = [];
+    const [tasks, setValue] = useState(initialState);
+
+    const onKeyCapture = (event: KeyboardEvent<HTMLInputElement>) => {
+        const currentTarget = event.currentTarget;
+        if (event.key === 'Enter') {
+            setValue([...tasks, currentTarget.value]);
+            currentTarget.value = '';
+        }
+    }
+
+    return <div>
+        <input name="task" type='text' onKeyDownCapture={onKeyCapture}
+               required/>
+        {tasks && tasks.length > 0 &&
+        <ul id="items">
+            {tasks.map((task: string) => <li key={task}>{task}</li>)}
+        </ul>
+        }
+    </div>;
+}
